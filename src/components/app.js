f9a44e3b2b6462db2c82d0bb69ebabca21fc095d
Ryan Ghaida <ryaboug@gmail.com>
2019-10-22 14:09:55 -0400

    gitignore

--- src/components/app.js
+++ src/components/app.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route} from 'react-router-dom';
 import DashBoard from './DashBoard';
 import Home from './Home';
 
+
 export default class App extends Component {
   render() {
     return (
