0ca3d1a20751c82ef859362c1477a33dac83af86
Ryan Ghaida <ryaboug@gmail.com>
2019-11-05 18:54:50 -0500

    working on adding login/logout on navbar working

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,28 +1,43 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
-import ToolBar from '@material-ui/core/ToolBar';
-import Typography from  '@material-ui/core/Typography';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
 const NavBar = (props) => {
+  const classes = useStyles();
+
   return(
-    <div>
-      <AppBar position="static" >
-        <ToolBar> 
-          <Typography variant="h3" color="inherit">
-            Bug Logger
-          </Typography>
-          <Typography variant="h5" color="inherit">
-            Logged in as: {props.user.email}
-          </Typography>
-          <Button >
-            
-          </Button>
-        </ToolBar>
-      </AppBar> 
-    </div>
-  )
+    <div className={classes.root}>
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h4" className={classes.title}>
+          BugLogger
+        </Typography>
+        {props.user ? <Button color="inherit" onClick={()=> props.handleLogout()}>Logout</Button>: <Button color="inherit">Login</Button> }
+        
+      </Toolbar>
+    </AppBar>
+  </div>
+  );
 }
 
 export default NavBar;
